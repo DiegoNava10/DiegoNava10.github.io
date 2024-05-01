e20be8f1988b6b64861d57c69dc964a1b5c94c80
NoeEz <136865416+NoeEz@users.noreply.github.com>
2024-05-01 15:03:50 -0600

    Calendario
    Correciones

--- src/app/renta/renta.component.ts
+++ src/app/renta/renta.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalendarModule } from 'primeng/calendar';
 @Component({
   selector: 'app-renta',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,CalendarModule],
   templateUrl: './renta.component.html',
   styleUrl: './renta.component.css'
 })
