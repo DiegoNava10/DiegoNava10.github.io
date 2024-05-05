bfa8d73ba75d299759339e0c27dc08bd1b693392
DiegoNava10 <151683046+DiegoNava10@users.noreply.github.com>
2024-05-04 20:25:19 -0600

    Se agrego las alerts y uso de Ang Material
    Se agrego las alerts y uso de Ang Material

--- src/app/renta/renta.component.ts
+++ src/app/renta/renta.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalendarModule } from 'primeng/calendar';
 import { AutosComponent } from '../autos/autos.component';
@@ -6,12 +6,15 @@ import { Auto, Cita } from '../auto';
 import { AutoService } from '../servicio/auto.service';
 import { FormsModule } from '@angular/forms';
 import { CitasService } from '../citas.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-renta',
   standalone: true,
   imports: [CommonModule,CalendarModule,AutosComponent,FormsModule],
   templateUrl: './renta.component.html',
-  styleUrl: './renta.component.css'
+  styleUrl: './renta.component.css',
 })
 export class RentaComponent {
   cita!: Cita;
@@ -23,15 +26,34 @@ export class RentaComponent {
     "text-align":"center"
   }
   misAutos:Auto[]=[];
-  constructor(public autoservicio:AutoService, private citasService: CitasService){
+
+  
+
+  constructor(public autoservicio:AutoService, private citasService: CitasService, private _snackBar: MatSnackBar){
     console.log("AutosComponent constructor");
   }
   ngOnInit(): void{
     this.cita=this.citasService.nuevaCita();
     this.misAutos=this.autoservicio.getAutos();
   }
-  nuevaCita():void{
+  nuevaCita(mensaje: string, accion: string):void{
+    
     console.log(this.cita.nombre);
     this.citasService.agregarCita(this.cita);
+    Swal.fire('¡Felicidades!', 'Tu renta ha sido agendada', 'success');
+
+    this._snackBar.open(mensaje, accion, {
+      duration: 5000,
+      panelClass: ['estilo-snackbar'],
+    });
+    this.cita.nombre = '';
+    this.cita.apellido = '';
+    this.cita.telefono = '';
+    this.cita.correo = '';
+    this.cita.fecha = '';
+    this.cita.nomAuto = '';
+    this.cita.marca = '';
+    this.cita.modelo = '';
+    this.cita.diasRentados = '';
   }
 }
