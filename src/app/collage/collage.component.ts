2584149a129f8c4cddc82fc7e71146dee729c893
Edgar Miranda <al314354@edu.uaa.mx>
2024-05-06 19:56:45 -0600

    tooltip de primeng

--- src/app/collage/collage.component.ts
+++ src/app/collage/collage.component.ts
@@ -3,11 +3,12 @@ import { Auto } from '../auto';
 import { AutoService } from '../servicio/auto.service';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-collage',
   standalone: true,
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink,CommonModule,TooltipModule],
   templateUrl: './collage.component.html',
   styleUrl: './collage.component.css'
 })
